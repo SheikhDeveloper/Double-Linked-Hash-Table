0942d015f53629db9a0cf9eb9c5ac5545a44fcc7
Umirinbrah <95506079+Umirinbrah@users.noreply.github.com>
2022-03-20 22:40:30 +0300

    Add files via upload

---
+++ DLHshTable (2).py
@@ -0,0 +1,106 @@
+
+
+class Dict:
+  def __init__(self, table_size = 10):
+    self.table = [None, None, None, None] * table_size
+    self.len = 0
+    self.top = [None, None, None, None]
+
+  def __getitem__(self, key):
+    index = hash(key) % len(self.table)
+    #print(index)
+    print(self.table)
+    #print(self.top)
+
+    current_value = self.table[index]
+    #print(current_value)
+    
+    if current_value != None:
+      while current_value != None:
+        if current_value[2] == key: return current_value[3]
+        current_value = current_value[1]
+    else:
+      raise KeyError()
+      #print(1)
+
+  def __setitem__(self, key, value):
+    index = hash(key) % len(self.table)
+
+    current_value = self.table[index]
+
+    if current_value == None:
+      current_value = [self.top, None, key, value]
+      self.top[1] = current_value
+      current_value[0] = self.top
+      self.table[index] = current_value
+      top_index = hash(self.top[2]) % len(self.table)
+      self.table[top_index] = self.top
+      self.top = current_value
+      self.len += 1
+
+    else:
+      current_value[3] = value
+      self.top = current_value[0]
+      if self.top != None:
+        self.top[1] = current_value
+        top_index = hash(self.top[2]) % len(self.table)
+        self.table[top_index] = self.top
+      current_value[0] = self.top
+      #top_index = hash(self.top[2]) % len(self.table)
+      self.table[index] = current_value
+      #self.table[top_index] = self.top
+      self.top = current_value
+
+    if self.len * 2 > len(self.table): self.resize()
+
+  def __delitem__(self, key):
+    index = hash(key) % len(self.table)
+    
+    current_value = self.table[index]
+
+    if current_value != None:
+      while current_value != None:
+        if current_value[2] == key:
+          self.top = current_value[0]
+          current_value = current_value[1]
+          if current_value != None:
+            current_value[0] = self.top
+          else:
+            current_value = [self.top, None, None, None]
+          self.top[1] = current_value
+          self.table[index] = [None]
+          current_index = hash(current_value[2]) % len(self.table)
+          top_index = hash(self.top[2]) % len(self.table)
+          self.table[current_index] = current_value
+          self.table[top_index] = self.top
+        prev_value = current_value
+        current_value = current_value[1]
+      self.top = prev_value
+      self.len -= 1
+    else:
+      raise KeyError()
+
+  def __contains__(self, key):
+    index = hash(key) % len(self.table)
+
+    current_value = self.table[index]
+    if current_value != None:
+      while current_value != None:
+        if current_value[2] == key: return True
+        current_value = current_value[1]
+      return False
+    else:
+      return False
+
+  def __len__(self):
+    return self.len
+
+  def resize(self):
+    raise RunTimeError('Not Implemented')
+
+    
+    
+      
+    
+        
+
